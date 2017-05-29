999de1b4470a2615c59735e9969927c9659ad13f
Nazmus Shakib <mdshuvobc@gmail.com>
2017-05-29 13:58:09 +0600

    added new fnc.

--- detectModifiedFile.php
+++ detectModifiedFile.php
@@ -1,37 +1,36 @@
 <?php
-
 class checkFile {
-	private $f = 'myfile.txt';
-	private $t = 'time.txt';
-	private $dateFormat = "F d Y H:i:s"; // "D d M Y g:i A"
-	private $mtime = '';
+	protected $f;
+	private $mtime;
+	private static $t = 'time.txt';
+	public $dateFormat = "F d Y H:i:s"; // "D d M Y g:i A"
 
-	function __construct() {
-		if(!file_exists($this->f))
-		$handle = fopen($this->f, 'w') or die('Cannot open file:  '.$this->f);
-		if(!file_exists($this->t))
-		$handle = fopen($this->t, 'w') or die('Cannot open file:  '.$this->t);
+	function __construct($txtFile) {
+		$this->f = $txtFile;
 		$this->mtime = filemtime($this->f);
+
+		if(!file_exists(self::$t))
+		$handle = fopen(self::$t, 'w') or die('Cannot open file:  '.self::$t);
 	}
 
-	private function getLastModTime($t) {
+	public function getLastModTime($t) {
 		$file_handle = fopen($t, "r");
 		while (!feof($file_handle)) {
-			return $line = fgets($file_handle);
+			return $time = fgets($file_handle);
 		}
 	}
 
 	public function getResult() {
-		if ($this->getLastModTime($this->t) != $this->mtime) {
+		if ($this->getLastModTime(self::$t) != $this->mtime) {
 		  echo "$this->f was modified! At: ".date($this->dateFormat, $this->mtime);
-		  $file_handle = fopen($this->t, 'w');
+		  $file_handle = fopen(self::$t, 'w');
 			fwrite($file_handle, $this->mtime);
 			fclose($file_handle);
 		} else {
-			echo "$this->f Not modified! Last Time: ".date($this->dateFormat, $this->getLastModTime($this->t));
+			echo "$this->f Not modified! Last Time: ".date($this->dateFormat, $this->getLastModTime(self::$t));
 		}
 	}
 }
 
-$checkFile = new checkFile();
+$checkFile = new checkFile('myfile.txt');
 $checkFile->getResult();
